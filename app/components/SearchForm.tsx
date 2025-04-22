d35fc6da77adfe783a8740daf2b5a9dd24c69f9d
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-04-22 21:25:38 +0100

    assets

---
+++ app/components/SearchForm.tsx
@@ -0,0 +1,11 @@
+import Form from 'next/form'
+
+const SearchForm = () => {
+  return (
+    <Form action='/' scroll={false} className='search-form'>
+
+    </Form>
+  )
+}
+
+export default SearchForm
