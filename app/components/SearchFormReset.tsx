1b6ded0e46e89492cbaff7ad80c9536cd12c6ef0
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-04-28 03:34:55 +0100

    search form

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const SearchFormReset = () => {
+  const router = useRouter();
+  const reset = () => {
+    const form = document.querySelector('.search-form') as HTMLFormElement;
+    if (form) form.reset();
+    router.push("/");
+  }
+  return (
+    <button type='button' onClick={reset} className="search-btn text-white">
+      <X className="size-5" />
+    </button>
+  )
+}
+
+export default SearchFormReset
+
+
