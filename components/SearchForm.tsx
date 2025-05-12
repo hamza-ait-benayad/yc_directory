8a7bdc9f834ceacd216d6750687afd99abf37bcd
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-05-12 23:33:09 +0100

    search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -8,18 +8,21 @@ const SearchForm = ({ query }: { query?: string }) => {
 
   return (
     <Form action="/" scroll={false} className="search-form">
+
       <input
         name="query"
         defaultValue={query}
         className="search-input"
         placeholder="Search Startups"
       />
+
       <div className="flex gap-2">
         {query && <SearchFormReset />}
         <button type="submit" className="search-btn text-white">
-          <Search className="size-5"/>
+          <Search className="size-5" />
         </button>
       </div>
+      
     </Form>
   );
 };
